ce8a85e6e49643e922206df1c77fdf1c2652e07c
h-des <patryk.holysz@gmail.com>
2017-12-21 22:23:11 +0100

    add proxy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
+  callbackURL: 'https://emaily-server-course.herokuapp.com/auth/google/callback',
+  proxy: true
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleID: profile.id }).then((existingUser) => {
     if (existingUser) {
